62c3b362e777b59a819c46d3e3aca1827c5cd1f1
MichaelChangeChen <MichaelChangeChen@users.noreply.github.com>
2025-08-12 06:20:07 +0000

    deploy: 34e23014db6f9bc9de80688cc0cac6a6eb6a4481

---
+++ assets/getApi-CTtJ8rnf.js
@@ -0,0 +1,1 @@
+import{l as e}from"./index-Clq9lwW9.js";e.defaults.timeout=6e4;const s=e.create({baseURL:"http://127.0.0.1:8000"}),o=t=>s.post("/guessGame",t),r=()=>s.get("/resetGame");export{o as g,r};
