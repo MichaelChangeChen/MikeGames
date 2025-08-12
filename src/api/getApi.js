5fbf1095bd2d06c921a102165f6e25ebae897f18
MichaelChangeChen <michaelexgt@gmail.com>
2025-08-12 16:18:33 +0800

    set api baseURL

--- src/api/getApi.js
+++ src/api/getApi.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 axios.defaults.timeout = 60000;
 
-// ==================================================
+const isLocal = window.location.hostname === "127.0.0.1";
 
 const request = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: window.location.hostname === "127.0.0.1" ? 'http://127.0.0.1:8000' : 'https://pythonbackend-davincicode.onrender.com',
 });
 
-export const guessGame = (data) => request.post(`/guessGame`, data); 
+export const guessGame = (data) => request.post(`/guessGame`, data);
 export const resetGame = () => request.get(`/resetGame`);
