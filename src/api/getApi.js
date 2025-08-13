753e8b5ed275ceb67a0656ce2a474661c6610c54
MichaelChangeChen <michaelexgt@gmail.com>
2025-08-13 18:49:46 +0800

    api use isLocal to check local or onrender

--- src/api/getApi.js
+++ src/api/getApi.js
@@ -5,7 +5,7 @@ axios.defaults.timeout = 60000;
 const isLocal = window.location.hostname === "127.0.0.1";
 
 const request = axios.create({
-  baseURL: window.location.hostname === "127.0.0.1" ? 'http://127.0.0.1:8000' : 'https://pythonbackend-davincicode.onrender.com',
+  baseURL: isLocal ? 'http://127.0.0.1:8000' : 'https://pythonbackend-davincicode.onrender.com',
 });
 
 export const guessGame = (data) => request.post(`/guessGame`, data);
