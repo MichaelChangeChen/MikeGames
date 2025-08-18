07219437b6ec8c1df531b95397ff6468dcfb33f0
MichaelChangeChen <michaelexgt@gmail.com>
2025-08-18 18:27:09 +0800

    api conect pythonbackend

--- src/api/getApi.js
+++ src/api/getApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 axios.defaults.timeout = 60000;
-const request = axios.create({ baseURL: 'http://127.0.0.1:8000' });
-// const request = axios.create({ baseURL: 'https://pythonbackend-davincicode.onrender.com' });
+// const request = axios.create({ baseURL: 'http://127.0.0.1:8000' });
+const request = axios.create({ baseURL: 'https://pythonbackend-davincicode.onrender.com' });
 
 export const resetGame = () => request.get(`/resetGame`);
 export const getScore = () => request.get(`/getScore`);
